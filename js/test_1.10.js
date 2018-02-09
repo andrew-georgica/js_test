e213f3c54e19fb13464a19e93433b490f1ee9e97
Andrii Georgica <georgica.andrii@gmail.com>
2018-02-09 15:18:57 +0200

    Change test_1.10.js

---
+++ js/test_1.10.js
@@ -0,0 +1,14 @@
+
+function quack(num) {
+  for (var i = 0; i <= num; i++) {
+    console.log(i);
+  }
+}
+
+var fly = function (num) {
+  for (var i = 0; i < num; i++) {
+    console.log("Flying!");
+  }
+}
+
+fly(3);
